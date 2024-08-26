15caaf22fa2f5066b2dad335ef7b38617fc0cecb
kwon5700 <kwon777071@gmail.com>
2024-08-27 00:58:56 +0900

    Feat : ID 에 해당하는 게시물 읽기 기능 개발

--- back.js
+++ back.js
@@ -20,17 +20,35 @@ const BLOG_data = []
 
 app.post('/post', function(req, res) {  // 1. 게시물 업로드 기능
     const {title_data, content_data} = req.body
+    const id_data = BLOG_data.length
 
     BLOG_data.push({
+        id : id_data+1,
         title : title_data,
         content : content_data
     })
 
-    res.send('200')
+    res.status(200).send('Upload is done!')
 })
 
-app.get('/get', function(req, res) { 
+app.get('/getAll', function(req, res) { // 2. 게시물 전체 읽기 기능
     
     res.json(BLOG_data)
 })
 
+app.get('/getById', function(req, res) { // 2. ID 에 해당하는 게시물 읽기 기능
+    const id_data = parseInt(req.query.id)
+    const idx = BLOG_data.find(idx => idx.id === id_data)
+
+    if(idx) {
+        res.json(idx)
+    } else {
+        res.status(404).send('No data')
+    }
+})
+
+app.patch('/patch', function(req,res) { // 3. 게시물 수정 기능
+    const modify_id = req.body
+
+
+})
