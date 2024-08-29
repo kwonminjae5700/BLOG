602f9848464e0eb3d4d75cb5c53c8206bf446741
kwon5700 <kwon777071@gmail.com>
2024-08-29 09:03:07 +0900

    Fix : cors 도메인 문제 해결

--- back.js
+++ back.js
@@ -8,9 +8,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
     console.log("start! blog server is open!!")
